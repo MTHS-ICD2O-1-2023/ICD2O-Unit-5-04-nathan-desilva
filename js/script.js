2811ae6caa94a3163851cbdb5caf00da575cc1c7
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-04-30 18:30:42 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  // input
+  const age = parseInt(document.getElementById('age-entered').value)
+  const day = document.getElementById('day-selected').value
+
+  // process
+  if (day == "Tuesday" || day == "Thursday" || age > 12 && age < 21) {
+    document.getElementById('answer').innerHTML = "You get the student discount."
+  } else {
+    document.getElementById('answer').innerHTML = "You must pay regular price."
+  }
+}
