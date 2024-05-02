b63ed449ab6d41b37b35ce2161847ba41db732b6
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-05-02 18:36:22 +0000

    changed const

--- js/script.js
+++ js/script.js
@@ -9,10 +9,10 @@
 function myButtonClicked() {
   // input
   const age = parseInt(document.getElementById('age-entered').value)
-  const dayButton = document.getElementById('option-1').checked
+  const day = document.getElementById('option-1').checked
 
   // process
-  if (dayButton == "Tuesday" || dayButton == "Thursday"|| age > 12 && age < 21) {
+  if (day == "Tuesday" || day == "Thursday"|| age > 12 && age < 21) {
     document.getElementById('answer').innerHTML = "You get the student discount."
   } else {
     document.getElementById('answer').innerHTML = "You must pay regular price."
